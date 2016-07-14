f983f1f2923083e257ca67c6d1eb31f66f39b620
Njagi Mwaniki <njagi@urbanslug.com>
2016-07-14 18:40:23 +0300

    Fix broken addRep and subRep in reputation.

--- src/Bot/Reputation.hs
+++ src/Bot/Reputation.hs
@@ -18,42 +18,38 @@ import Prelude hiding ((.), id)   -- we use (.) and id from `Control.Category`
 import Bot.Types
 
 
-
 repBot :: Monad m => RoomBot m
 repBot = proc (InMessage nick msg _ _) -> do
-    updateB <- updateBlips -< (nick, msg)
 
-    reps    <- trackReps   -< updateB
+    queryB  <- queryBlips  -< (nick, msg)
 
-    queryB  <- queryBlips  -< msg
+    reps    <- trackReps   -< queryB
 
-    let lookupRep :: Nick -> [Message]
-        lookupRep nick' =
+    let lookupRep :: (Nick, Int) -> [Message]
+        lookupRep (nick', _) =
           case nick' of
-           ""        -> ["Done."]
+           "Insult"        -> ["Operation not allowed, dumbass!"]
            _ -> [nick ++ " has a reputation of " ++ show rep ++ "."]
            where
              rep = M.findWithDefault 0 nick reps
 
     id -< lookupRep <$> queryB
   where
-    updateBlips :: Auto m (Nick, Message) (Blip (Nick, Int))
-    updateBlips = emitJusts getUpdateCommand
+    queryBlips :: Auto m (Nick, Message) (Blip (Nick, Int))
+    queryBlips =  emitJusts getRequest
       where
-        -- updater is the person triggering the update blip
-        getUpdateCommand (updater, msg) =
-          case words msg of
-            "@addRep":nick:_ | nick /= updater -> Just (nick, 1)
-            "@subRep":nick:_                   -> Just (nick, -1)
-            _                                  -> Nothing
-    
-    trackReps :: Monad m => Auto m (Blip (Nick, Int)) (Map Nick Int)
+        getRequest (nick, msg) =
+          let (trigger:nick':_) = words msg
+
+          in if nick' == nick
+             then Just ("Insult", 0)
+             else case trigger of
+              "@addRep" -> Just (nick', 1)
+              "@subRep" -> Just (nick', -1)
+              "@rep"    -> Just (nick', 0)
+              _         -> Nothing
+
+
+    trackReps :: Auto m (Blip (Nick, Int)) (Map Nick Int)
     trackReps = scanB (\mp (nick, change) -> M.insertWith (+) nick change mp) M.empty
-    
-    queryBlips :: Auto m Message (Blip Nick)
-    queryBlips = emitJusts (getRequest . words)
-      where
-        getRequest ("@addRep":_)   = Just ""
-        getRequest ("@subRep":_)   = Just ""
-        getRequest ("@rep":nick:_) = Just nick
-        getRequest _               = Nothing
+
